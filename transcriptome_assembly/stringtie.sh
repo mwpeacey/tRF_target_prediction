c29705e5189de09ad2e448b5d66047aa8c01ae4d
Peacey <mpeacey@bamdev1.cm.cluster>
2025-02-05 13:57:46 -0500

    Stringtie assembly scripts

---
+++ transcriptome_assembly/stringtie.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+#$ -cwd
+#$ -pe threads 4
+#$ -l m_mem_free=16G
+
+## Description
+## Assembles a GTF annotation from RNA-seq data using stringtie with conservative settings.
+## Designed for use with stringtie_wrapper.sh
+
+## Requirements
+## stringtie v2.2.1 (conda environment stringtie)
+
+echo "#########################"
+echo "New run started on $(date)"
+echo "#########################"
+
+DATA_DIRECTORY=$1
+ALIGNMENT_DIRECTORY=$2
+REFERENCE=$3
+SAMPLE=$4
+SAMPLE_NAME=$5
+STRANDEDNESS=$6
+
+cd ${ALIGNMENT_DIRECTORY}
+
+echo "Data directory : ${DATA_DIRECTORY}"
+echo "Alignment directory : ${ALIGNMENT_DIRECTORY}"
+echo "Sample name : ${SAMPLE_NAME}"
+echo "Strandedness : ${STRANDEDNESS}"
+
+if [ ${STRANDEDNESS} -eq 0 ]
+then
+
+	stringtie -o ${DATA_DIRECTORY}/stringtie/${SAMPLE_NAME}_stringtie-output.gtf \
+	 	  -G ${REFERENCE} \
+	  	  -p 4 -c 2 -f 0.05 -j 2 -s 5 \
+          	  ${SAMPLE}
+
+elif [ ${STRANDEDNESS} -eq 1 ]
+then
+
+	stringtie -o ${DATA_DIRECTORY}/stringtie/${SAMPLE_NAME}_stringtie-output.gtf \
+                  -G ${REFERENCE} \
+                  -p 4 -c 2 -f 0.05 -j 2 -s 5 \
+	 	  --fr \
+                  ${SAMPLE}
+
+elif [ ${STRANDEDNESS} -eq 2 ]
+then
+
+	stringtie -o ${DATA_DIRECTORY}/stringtie/${SAMPLE_NAME}_stringtie-output.gtf \
+                  -G ${REFERENCE} \
+                  -p 4 -c 2 -f 0.05 -j 2 -s 5 \
+                  --rf \
+                  ${SAMPLE}
+
+fi
+
+echo "Finished run on $(date)"
