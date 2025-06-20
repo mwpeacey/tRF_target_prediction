605952cc9032ad46f669c20b871b8a79f2e00011
Matthew Peacey <mpeacey@LWWCPRSS06.cshl.edu>
2025-06-20 19:06:10 -0400

    Annotate targets

--- R_scripts/annotate_targets.R
+++ R_scripts/annotate_targets.R
@@ -17,7 +17,7 @@ library(GenomicFeatures)
 
 data = read.csv(file = 'import/miranda/miranda_output_70.csv', header = TRUE) %>%
   dplyr::rename(transcript_start = start, transcript_end = end, start = genome_start, end = genome_end) %>%
-  dplyr::filter(alignment_score >= 80)
+  dplyr::filter(alignment_score >= 70)
 
 # Add gene information 
 
@@ -36,7 +36,10 @@ tRF_infomation = read.csv('import/mm10_tRF3b.csv') %>%
 
 data$tRF = stringr::str_remove(data$tRF, '>')
 
-data = merge(data, tRF_infomation, by = 'tRF')
+data = merge(data, tRF_infomation, by = 'tRF') %>%
+  separate(col = source_tRNAs, sep = '-', into = c('A', 'B', 'C', 'D'), remove = F) %>%
+  unite(tRNA_anticodon, c('B', 'C'), remove = F, sep = '-') %>%
+  dplyr::select(-c('A', 'B', 'C', 'D'))
 
 ################################################################################
 ## Annotate hit overlap with repeats
@@ -58,18 +61,45 @@ query = GRanges(data)
 LTR_overlap = findOverlaps(query = query, subject = subject, type = 'any', ignore.strand = T)
 LTR_overlap = as.data.frame(LTR_overlap)
 
-# Permutation analysis
-library(regioneR)
+# Add LTR indicator to data
 
-GTF = makeTxDbFromGFF(file = 'import/transcriptome_assembly/stringtie_merged_filtered.gtf')
-transcriptome_background = reduce(exons(GTF))
-seqlevelsStyle(transcriptome_background) = 'UCSC'
+data = data %>%
+  dplyr::mutate(LTR = dplyr::row_number() %in% LTR_overlap$queryHits)
 
-perm_results = overlapPermTest(
-  A = subject,
-  B = query,
-  ntimes = 20,
-  genome = 'mm10', 
-  universe=transcriptome_background ,
-  verbose = T)
+# Initialize LTR family vector
+LTR_family = vector(length = nrow(data))
+
+# Assign LTR family based on overlaps
+for (row in 1:nrow(data)) {
+  if (data[row, 'LTR']) {
+    subject_row = LTR_overlap$subjectHits[LTR_overlap$queryHits == row][1]
+    LTR_family[row] = subject[subject_row,]$family_id
+  } else {
+    LTR_family[row] = NA
+  }
+}
+
+data$LTR_family = LTR_family
+
+################################################################################
+## Heatmap
+################################################################################
+
+library(pheatmap)
+
+input = dplyr::filter(data, LTR == T) %>%
+  group_by(tRNA_anticodon, LTR_family) %>%
+  summarize(n = n()) %>%
+  pivot_wider(names_from = 'tRNA_anticodon', values_from = 'n') %>%
+  as.data.frame()
+
+input[is.na(input)] = 0
+
+row.names(input) = input$LTR_family
+
+input = dplyr::select(input, -c('LTR_family'))
+
+input = log10(input+0.01)
+
+pheatmap(input, cluster_col = T, cluster_row = T)
 
