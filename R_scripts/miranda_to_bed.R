7d653e2a2eb34b52358d2948e62cb7574d39b136
Matthew Peacey <mpeacey@Matty-P.cshl.edu>
2025-07-03 12:33:39 -0400

    Re-written to accomodate genomic alignment

--- R_scripts/miranda_to_bed.R
+++ R_scripts/miranda_to_bed.R
@@ -13,7 +13,7 @@ library(glue)
 ################################################################################
 
 filenames = list.files(glue("import/miranda"),
-                       pattern="*summary", 
+                       pattern="summary*", 
                        full.names=TRUE)
 
 counter = 1
@@ -49,79 +49,50 @@ score_cutoff = 70
 
 miranda_output = data %>%
   dplyr::rename('tRF' = 'V1', 
-                'target_id' = 'V2', 
+                'coordinates' = 'V2', 
                 'alignment_score' = 'V3',
                 'energy' = 'V4',
                 'Z-score' = 'V5',
                 'miRNA_position' = 'V6',
                 'target_position' = 'V7',
-                'alignment_length' = 'V8') %>%
+                'alignment_length' = 'V8',
+                'strand' = 'V11') %>%
   dplyr::filter(alignment_score >= score_cutoff)
 
-# For each transcript, takes the hit with the highest alignment score (deprecated)
-
-# miranda_output = miranda_output %>%
-#  group_by(tRF, target_id) %>%
-#  dplyr::slice(which.max(alignment_score))
-
-# Find start and end positions of the hit on the transcript
+# Find start and end positions of the hit in the window
 
 miranda_output = separate(miranda_output, 'target_position', c('start', 'end'))
 
-# Temporary fix to remove weird target_id entries
+miranda_output$start = as.numeric(miranda_output$start)
+miranda_output$end = as.numeric(miranda_output$end)
 
-miranda_output = miranda_output[grepl('ENSMUS', miranda_output$target_id) | grepl('MSTRG', miranda_output$target_id), ]
+# Find start and end coordinates of the window
 
-################################################################################
-# Adds genomic coordinates of hit sites
-################################################################################
+miranda_output = separate(miranda_output, 'coordinates', c('A', 'B'), sep = '::') %>%
+  separate('B', c('seqnames', 'C'), sep = ':') %>%
+  separate('C', c('window_start', 'window_end'), sep = '-')
 
-# Import GTF annotation as TxDb object
+miranda_output$window_start = as.numeric(miranda_output$window_start)
+miranda_output$window_end = as.numeric(miranda_output$window_end)
 
-transcriptome_TxDb = makeTxDbFromGFF(file="import/transcriptome_assembly/stringtie_merged_filtered.gtf",
-                                     dataSource='Stringtie assembled transcriptome combined with GENCODE vM23.',
-                                     organism='Mus musculus')
+# Determine genomic coordinates of hit site
 
-exons_by_transcript = exonsBy(transcriptome_TxDb, "tx", use.names=TRUE)
+miranda_output = mutate(miranda_output, genomic_start = window_start + start, genomic_end = window_start + end)
 
-# Filters miranda output to ensure all hit transcripts are in the GTF file.
-# They may be absent if e.g. the GTF file has been filtered after the
-# miranda run.
+# Remove duplicate hits that occur within window overlaps
 
-miranda_output = miranda_output[miranda_output$target_id %in% names(exons_by_transcript), ]
+miranda_output = distinct(miranda_output, tRF, seqnames, genomic_start, genomic_end, strand, .keep_all = TRUE)
 
-# Convert miranda hit coordinates to GRanges object
+# Cleanup 
 
-rng_tx = IRanges(start = as.numeric(miranda_output$start), 
-                 end = as.numeric(miranda_output$end),
-                 names = miranda_output$target_id)
-
-miranda_GRanges = GRanges(seqnames=miranda_output$target_id, ranges=rng_tx, strand = "*")
-
-# Map transcript coordinates to genome
-
-genome_coordinates = mapFromTranscripts(miranda_GRanges, exons_by_transcript)
-remove(miranda_GRanges)
-
-# Combine with original miranda output
-
-df = as.data.frame(genome_coordinates, 
-                   row.names = genome_coordinates$xHits)
-
-df$target_id = names(genome_coordinates)
-
-miranda_output$genome_start = df$start
-miranda_output$genome_end = df$end
-miranda_output$seqnames = df$seqnames
-miranda_output$strand = df$strand
-
-remove(df)
-
-# Filter to remove exon junction spanning hits
-
-miranda_output = miranda_output %>%
-  mutate(hit_length = as.numeric(genome_end) - as.numeric(genome_start) + 1) %>%
-  filter(hit_length <= 40)
+miranda_output = dplyr::select(miranda_output, c('tRF', 
+                                                 'seqnames', 
+                                                 'alignment_score', 
+                                                 'energy', 
+                                                 'genomic_start', 
+                                                 'genomic_end', 
+                                                 'alignment_length', 
+                                                 'strand'))
 
 ################################################################################
 # Export
@@ -134,8 +105,8 @@ write_csv(miranda_output, file = glue('import/miranda/miranda_output_{score_cuto
 ## Write bed
 
 miranda_bed = miranda_output %>%
-  dplyr::select(c('seqnames', 'genome_start', 'genome_end', 'tRF', 'strand')) %>%
-  dplyr::rename(chrom = 'seqnames', chromStart = 'genome_start', chromEnd = 'genome_end', name = 'tRF') 
+  dplyr::select(c('seqnames', 'genomic_start', 'genomic_end', 'tRF', 'strand', 'alignment_score')) %>%
+  dplyr::rename(chrom = 'seqnames', chromStart = 'genomic_start', chromEnd = 'genomic_end', name = 'tRF', score = 'alignment_score') 
 
 rtracklayer::export.bed(con = glue("import/miranda/miranda_output_{score_cutoff}.bed"), 
                         object = GenomicRanges::makeGRangesFromDataFrame(miranda_bed, keep.extra.columns = T),
