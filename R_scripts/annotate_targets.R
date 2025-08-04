fbf06a71fc9368a9c4e92452834c192bfa616b92
Matthew Peacey <mpeacey@Mac.cshl.edu>
2025-08-04 14:00:39 -0400

    Updated repeatmasker source

--- R_scripts/annotate_targets.R
+++ R_scripts/annotate_targets.R
@@ -42,87 +42,186 @@ data = merge(data, tRF_infomation, by = 'tRF') %>%
   dplyr::select(-c('A', 'B', 'C', 'D'))
 
 ################################################################################
+## Annotate hit overlap with repeats
+################################################################################
+
+# Load and filter LTR annotation
+LTR = read.csv('import/annotation_tables/UCSC_mm10_LTR.csv', header = T)
+LTR = LTR[!grepl('int', LTR$repName),]
+
+# Extend LTR elements by 200bp downstream
+subject = makeGRangesFromDataFrame(LTR, 
+                                   keep.extra.columns = TRUE,
+                                   start.field = 'genoStart',
+                                   end.field = 'genoEnd',
+                                   seqnames.field = 'genoName')
+end(subject[strand(subject) == '+']) = end(subject[strand(subject) == '+']) + 200
+start(subject[strand(subject) == '-']) = start(subject[strand(subject) == '-']) - 200
+
+# Query object from your data
+query = GRanges(data)
+
+# Find overlaps
+hits = findOverlaps(query, subject, type = 'any', ignore.strand = TRUE)
+hits_df = as.data.frame(hits)
+
+# Extract the overlapping metadata
+matched_LTR = subject[hits_df$subjectHits]
+overlap_info = data.frame(
+  queryHits = hits_df$queryHits,
+  LTR_family = mcols(matched_LTR)$repFamily,
+  LTR_gene_id = mcols(matched_LTR)$repName
+)
+
+# Keep only the first match per query (or you can customize to collapse all hits)
+overlap_info = overlap_info[!duplicated(overlap_info$queryHits), ]
+
+# Add LTR columns to main data
+data = data %>%
+  dplyr::mutate(row_id = row_number()) %>%
+  left_join(overlap_info, by = c("row_id" = "queryHits")) %>%
+  dplyr::mutate(LTR = !is.na(LTR_family)) %>%
+  dplyr::select(-row_id)  # clean up temporary column
+
+################################################################################
 ## Annotate hit overlap with GENCODE transcripts
 ################################################################################
 
-#Build GRanges of hits
-gr_hits = makeGRangesFromDataFrame(
+# Build GRanges of hits
+gr_hits = GenomicRanges::makeGRangesFromDataFrame(
   data,
-  seqnames.field   = "seqnames",
-  start.field      = "start",
-  end.field        = "end",
-  strand.field     = "strand",
+  seqnames.field = "seqnames",
+  start.field = "start",
+  end.field = "end",
+  strand.field = "strand",
   keep.extra.columns = TRUE
 )
 
 # Load annotation 
-ah  = AnnotationHub()
+ah = AnnotationHub::AnnotationHub()
 txdb = ah[["AH75036"]]
 
 # Extract feature GRanges
-gr_tx     = transcripts(txdb)
-gr_exons  = exons(txdb)
-gr_cds    = cdsBy(txdb, by="tx") %>% unlist()
-gr_utr5   = fiveUTRsByTranscript(txdb) %>% unlist()
-gr_utr3   = threeUTRsByTranscript(txdb) %>% unlist()
+gr_tx    = GenomicFeatures::transcripts(txdb)
+gr_exons = GenomicFeatures::exons(txdb)
+gr_cds   = GenomicFeatures::cdsBy(txdb, by = "tx") %>% unlist()
+gr_utr5  = GenomicFeatures::fiveUTRsByTranscript(txdb) %>% unlist()
+gr_utr3  = GenomicFeatures::threeUTRsByTranscript(txdb) %>% unlist()
 
 # Harmonize seqlevel styles
-seqlevelsStyle(gr_hits)  = "UCSC"
-seqlevelsStyle(gr_tx)    = "UCSC"
-seqlevelsStyle(gr_exons) = "UCSC"
-seqlevelsStyle(gr_cds)   = "UCSC"
-seqlevelsStyle(gr_utr5)  = "UCSC"
-seqlevelsStyle(gr_utr3)  = "UCSC"
+GenomeInfoDb::seqlevelsStyle(gr_hits)   = "UCSC"
+GenomeInfoDb::seqlevelsStyle(gr_tx)     = "UCSC"
+GenomeInfoDb::seqlevelsStyle(gr_exons)  = "UCSC"
+GenomeInfoDb::seqlevelsStyle(gr_cds)    = "UCSC"
+GenomeInfoDb::seqlevelsStyle(gr_utr5)   = "UCSC"
+GenomeInfoDb::seqlevelsStyle(gr_utr3)   = "UCSC"
 
-gr_exonsByTx = exonsBy(txdb, by="tx")
-seqlevelsStyle(gr_exonsByTx) = "UCSC"
+gr_exonsByTx = GenomicFeatures::exonsBy(txdb, by = "tx")
+GenomeInfoDb::seqlevelsStyle(gr_exonsByTx) = "UCSC"
 
-gr_exon_tx <- unlist(gr_exonsByTx)
-# capture the transcript ID in a metadata column
-mcols(gr_exon_tx)$tx_id = rep(names(gr_exonsByTx),
-                               elementNROWS(gr_exonsByTx))
-
-# 2) map each hit to any overlapping exon → transcript
-mcols(gr_hits)$hit_idx = seq_along(gr_hits)
-ex_ol = findOverlaps(gr_hits, gr_exon_tx, type="within")
-
-tx_map = tibble(
-  hit_idx    = mcols(gr_hits)$hit_idx[queryHits(ex_ol)],
-  transcript = mcols(gr_exon_tx)$tx_id[subjectHits(ex_ol)]
-) %>%
-  group_by(hit_idx) %>%
-  summarize(transcript = paste(unique(transcript), collapse = ";"), .groups="drop")
-
-# 3) Build your logical annotations as before
-annot = tibble(
-  hit_idx       = mcols(gr_hits)$hit_idx,
-  overlaps_5utr  = overlapsAny(gr_hits, gr_utr5),
-  overlaps_3utr  = overlapsAny(gr_hits, gr_utr3),
-  overlaps_cds   = overlapsAny(gr_hits, gr_cds),
-  overlaps_exon  = overlapsAny(gr_hits, gr_exons)  # unstranded exons
+gr_exon_tx = unlist(gr_exonsByTx)
+S4Vectors::mcols(gr_exon_tx)$tx_id = rep(
+  names(gr_exonsByTx),
+  S4Vectors::elementNROWS(gr_exonsByTx)
 )
 
-# 4) Stitch transcript names into annot, then join into your data
-annot = annot %>%
-  left_join(tx_map, by="hit_idx")
+# Map each hit to overlapping exon → transcript
+S4Vectors::mcols(gr_hits)$hit_idx = seq_along(gr_hits)
+ex_ol = GenomicRanges::findOverlaps(gr_hits, gr_exon_tx, type = "within")
 
-data <- data %>%
-  bind_cols(annot %>% dplyr::select(-hit_idx)) %>%
-  mutate(
-    gencode_location = case_when(
+tx_map = dplyr::tibble(
+  hit_idx = S4Vectors::mcols(gr_hits)$hit_idx[S4Vectors::queryHits(ex_ol)],
+  transcript = S4Vectors::mcols(gr_exon_tx)$tx_id[S4Vectors::subjectHits(ex_ol)]
+) %>%
+  dplyr::group_by(hit_idx) %>%
+  dplyr::summarize(transcript = paste(unique(transcript), collapse = ";"), .groups = "drop")
+
+# Region type annotations
+annot = dplyr::tibble(
+  hit_idx        = S4Vectors::mcols(gr_hits)$hit_idx,
+  overlaps_5utr  = IRanges::overlapsAny(gr_hits, gr_utr5),
+  overlaps_3utr  = IRanges::overlapsAny(gr_hits, gr_utr3),
+  overlaps_cds   = IRanges::overlapsAny(gr_hits, gr_cds),
+  overlaps_exon  = IRanges::overlapsAny(gr_hits, gr_exons)
+)
+
+# Join annotations
+annot = dplyr::left_join(annot, tx_map, by = "hit_idx")
+
+data = data %>%
+  dplyr::bind_cols(annot %>% dplyr::select(-hit_idx)) %>%
+  dplyr::mutate(
+    gencode_location = dplyr::case_when(
       overlaps_5utr ~ "Exon - 5' UTR",
       overlaps_3utr ~ "Exon - 3' UTR",
       overlaps_cds  ~ "Exon - CDS",
       overlaps_exon ~ "Exon - non-coding",
       TRUE          ~ "Intergenic"
-    ) 
+    )
   ) %>%
   dplyr::rename(gencode_transcripts = transcript) %>%
-  dplyr::select(
-    tRF, seqnames, alignment_score, energy, tRNA_anticodon,
-    start, end, alignment_length,
-    strand, gencode_location, gencode_transcripts
+  dplyr::select(-c('overlaps_5utr', 'overlaps_3utr', 'overlaps_cds',
+                   'overlaps_exon'))
+
+################################################################################
+## Add gene names and gene IDs directly from TxDb
+################################################################################
+
+# Build mapping from transcript → gene_id
+tx_to_gene = dplyr::tibble(
+  transcript_id = gr_tx$tx_name,
+  gene_id = gr_tx$gene_id
+)
+
+# Extract gene name from genes() metadata
+genes_gr = GenomicFeatures::genes(txdb)
+
+gene_metadata = S4Vectors::mcols(genes_gr) %>%
+  as.data.frame() %>%
+  dplyr::select(gene_id, gene_name)
+
+# Join gene_name to transcript info
+tx_to_gene = dplyr::left_join(tx_to_gene, gene_metadata, by = "gene_id")
+
+# Expand transcripts per row
+data = data %>% dplyr::mutate(row_id = dplyr::row_number())
+
+tx_long = data %>%
+  dplyr::select(row_id, gencode_transcripts) %>%
+  tidyr::separate_rows(gencode_transcripts, sep = ";")
+
+# Join gene_id + gene_name
+tx_long_annot = dplyr::left_join(
+  tx_long,
+  tx_to_gene,
+  by = c("gencode_transcripts" = "transcript_id")
+) %>%
+  dplyr::filter(!is.na(gene_name))
+
+# Collapse back to per-row
+gene_info_per_row = tx_long_annot %>%
+  dplyr::group_by(row_id) %>%
+  dplyr::summarize(
+    gencode_gene_id   = paste(unique(gene_id), collapse = ";"),
+    gencode_gene_name = paste(unique(gene_name), collapse = ";"),
+    .groups = "drop"
   )
+
+# Join into data
+data = data %>%
+  dplyr::left_join(gene_info_per_row, by = "row_id") %>%
+  dplyr::select(-row_id)
+
+################################################################################
+## Annotate Gag-like genes
+################################################################################
+
+scan_genes = read_csv('import/annotation_tables/mouse_scan_genes.csv')
+other_gag_genes = read_csv('import/annotation_tables/mouse_gag_genes.csv')
+
+data = dplyr::mutate(data, gag_gene = case_when(gencode_gene_id %in% scan_genes$gene_id ~ T,
+                                                gencode_gene_name %in% other_gag_genes$gene_name ~ T,
+                                                 T ~ F))
 
 ################################################################################
 ## Annotate overlap with StringTie transcripts
@@ -310,53 +409,8 @@ data <- data %>%
     )
   ) %>%
   dplyr::rename(stringtie_transcripts = transcript) %>%
-  dplyr::select(
-    tRF, seqnames, alignment_score, energy, tRNA_anticodon,
-    start, end, alignment_length,
-    strand, gencode_location, gencode_transcripts,
-    stringtie_location, stringtie_transcripts
-  )
-
-################################################################################
-## Annotate hit overlap with repeats
-################################################################################
-
-# Load and filter LTR annotation
-LTR = rtracklayer::readGFF('import/annotation_tables/mm10_rmsk_TE.gtf') %>%
-  dplyr::filter(class_id == 'LTR')
-
-LTR = LTR[!grepl('int', LTR$gene_id),]
-
-# Extend LTR elements by 200bp downstream
-subject = makeGRangesFromDataFrame(LTR, keep.extra.columns = TRUE)
-end(subject[strand(subject) == '+']) = end(subject[strand(subject) == '+']) + 200
-start(subject[strand(subject) == '-']) = start(subject[strand(subject) == '-']) - 200
-
-# Query object from your data
-query = GRanges(data)
-
-# Find overlaps
-hits = findOverlaps(query, subject, type = 'any', ignore.strand = TRUE)
-hits_df = as.data.frame(hits)
-
-# Extract the overlapping metadata
-matched_LTR = subject[hits_df$subjectHits]
-overlap_info = data.frame(
-  queryHits = hits_df$queryHits,
-  LTR_family = mcols(matched_LTR)$family_id,
-  LTR_gene_id = mcols(matched_LTR)$gene_id
-)
-
-# Keep only the first match per query (or you can customize to collapse all hits)
-overlap_info = overlap_info[!duplicated(overlap_info$queryHits), ]
-
-# Add LTR columns to main data
-data = data %>%
-  dplyr::mutate(row_id = row_number()) %>%
-  left_join(overlap_info, by = c("row_id" = "queryHits")) %>%
-  dplyr::mutate(LTR = !is.na(LTR_family)) %>%
-  dplyr::select(-row_id)  # clean up temporary column
-
+  dplyr::select(-c('overlaps_5utr', 'overlaps_3utr', 'overlaps_cds',
+                   'overlaps_exon'))
 
 ################################################################################
 ## Annotate target site hits with phast con scores
@@ -385,34 +439,124 @@ data = as.data.frame(scores)
 ################################################################################
 
 # Convert to GRanges
-gr = GRanges(
+gr = GenomicRanges::GRanges(
   seqnames = data$seqnames,
-  ranges = IRanges(start = data$start, end = data$end),
+  ranges = IRanges::IRanges(start = data$start, end = data$end),
   strand = data$strand
 )
 
-# Reduce overlapping ranges
-reduced_gr = reduce(gr)
+# Reduce overlapping regions
+reduced_gr = GenomicRanges::reduce(gr)
 
-# Find overlaps
-hits = findOverlaps(reduced_gr, gr)
-hits_df = as.data.frame(hits)
+# Find overlaps (mapping each reduced range to original hits)
+hits = GenomicRanges::findOverlaps(reduced_gr, gr)
+hits_df = tibble::as_tibble(hits)
 
-# Count how many original ranges map to each reduced range
-tRF_counts = hits_df %>%
-  group_by(queryHits) %>%
-  summarise(collapsed_tRF_count = n(), .groups = "drop")
+# Add original alignment_score column for ranking
+hits_df = hits_df %>%
+  dplyr::mutate(
+    alignment_score = data$alignment_score[subjectHits]
+  )
 
-# Pick the best hit for each reduced range
+# Identify best-scoring original hit for each reduced region
 best_hits = hits_df %>%
-  group_by(queryHits) %>%
-  slice_max(order_by = data$alignment_score[subjectHits], n = 1, with_ties = FALSE)
+  dplyr::group_by(queryHits) %>%
+  dplyr::slice_max(order_by = alignment_score, n = 1, with_ties = FALSE) %>%
+  dplyr::ungroup()
 
-# Extract best original rows
+# Count total hits per reduced region
+tRF_counts = hits_df %>%
+  dplyr::count(queryHits, name = "collapsed_tRF_count")
+
+# Merge best hit info with tRF counts
+best_hits = dplyr::left_join(best_hits, tRF_counts, by = "queryHits")
+
+# Extract rows from data
 unique_data = data[best_hits$subjectHits, ]
+unique_data$collapsed_tRF_count = best_hits$collapsed_tRF_count
 
-# Add the tRF count to unique_data
-unique_data$collapsed_tRF_count = tRF_counts$collapsed_tRF_count[match(best_hits$queryHits, tRF_counts$queryHits)]
+################################################################################
+## Exon distance analysis
+################################################################################
+
+#Build GRanges of hits
+
+gr_hits = makeGRangesFromDataFrame(
+  unique_data,
+  seqnames.field   = "seqnames",
+  start.field      = "start",
+  end.field        = "end",
+  strand.field     = "strand",
+  keep.extra.columns = TRUE
+)
+
+# Load annotation 
+
+ah  = AnnotationHub()
+txdb = ah[["AH75036"]]
+gr_tx = transcripts(txdb)
+gr_exons  = exonsBy(txdb, by = "tx")
+
+# Harmonize seqlevel styles
+seqlevelsStyle(gr_hits)  = "UCSC"
+seqlevelsStyle(gr_tx)    = "UCSC"
+seqlevelsStyle(gr_exons) = "UCSC"
+
+# Filter to protein-coding transcripts
+
+protein_coding_tx = gr_tx[gr_tx$tx_biotype == "protein_coding"]
+protein_coding_ids = protein_coding_tx$tx_name
+
+exons_by_tx_coding = gr_exons[names(gr_exons) %in% protein_coding_ids]
+
+# Get TSS positions of protein-coding transcripts
+
+tss_pc = promoters(protein_coding_tx, upstream = 1, downstream = 1)
+
+# Find overlaps with LTRs
+
+LTR = rtracklayer::readGFF('import/annotation_tables/mm10_rmsk_TE.gtf') %>%
+  dplyr::filter(class_id == 'LTR')
+
+ltr_gr = makeGRangesFromDataFrame(LTR, keep.extra.columns = TRUE)
+
+hits = findOverlaps(tss_pc, ltr_gr, ignore.strand = FALSE)
+
+ltr_initiated_tx = protein_coding_tx[queryHits(hits)]$tx_name
+
+exons_by_tx_ltr_coding = gr_exons[names(gr_exons) %in% ltr_initiated_tx]
+
+# Sort exons by their 5' end depending on strand
+
+first_splice_sites_list = lapply(exons_by_tx_ltr_coding, function(exs) {
+  strand_sign = as.character(strand(exs)[1])
+  exs_sorted = if (strand_sign == "+") exs[order(start(exs))] else exs[order(end(exs), decreasing = TRUE)]
+  
+  first_exon = exs_sorted[1]
+  pos = if (strand_sign == "+") end(first_exon) else start(first_exon)
+  
+  GRanges(
+    seqnames = seqnames(first_exon),
+    ranges = IRanges(start = pos, end = pos),
+    strand = strand(first_exon),
+    tx_name = names(exs)[1]
+  )
+})
+
+first_splice_sites = Reduce(c, first_splice_sites_list)
+
+# Compute nearest distance
+
+hits = distanceToNearest(gr_hits, first_splice_sites)
+
+# Attach distances back to your data
+
+gr_hits$distance_to_5p_splice = NA_integer_
+gr_hits$nearest_tx = NA_character_
+gr_hits$distance_to_5p_splice[queryHits(hits)] = mcols(hits)$distance
+#gr_hits$nearest_tx[queryHits(hits)] = first_splice_sites$tx_name[subjectHits(hits)]
+
+unique_data = as.data.frame(gr_hits)
 
 ################################################################################
 ## Export
